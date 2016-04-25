2f8b32b887b3c0bdc0d12a4b2cd000a7be578292
Anurag El Dorado <annurag94@gmail.com>
2016-04-25 19:23:14 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-// var sitelist = [];
-
 chrome.management.getAll(function(allExtensions) {
 	allExtensions.sort(function(left, right){
 	    var nameorder = left.name === right.name ? 0 : (left.name < right.name ? -1 : 1);
@@ -14,8 +12,10 @@ chrome.management.getAll(function(allExtensions) {
 	        return 1;
 	    }
 	});
+
 	allExtensions.forEach(function(extension) {
 		display(extension);
+		console.log(extension);
 	});
 });
 
@@ -25,19 +25,21 @@ function display(extension) {
 	var extensionDiv = document.createElement('div');
 	extensionDiv.classList.add(extension.enabled? 'extension-div-enabled': 'extension-div-disabled');
 
-	var nameDiv = document.createElement('div');
+	// create and append the name div
+	var nameDiv = document.createElement('div');	// <div class="name-div">Grammarly</div>
 	nameDiv.classList.add('name-div');
 	nameDiv.innerHTML = extension.name;
+	extensionDiv.appendChild(nameDiv);
 
+	// create the desc div
+	// <div class="desc">this extension does this this extension does this this extension does this</div>
 	var descDiv = document.createElement('div');
 	descDiv.classList.add('desc-div');
 	descDiv.setAttribute('title', extension.description);
-	descDiv.innerHTML = extension.description;
+	descDiv.innerHTML = extension.description.length >= 85 ? (extension.description.substring(0, 85) + '...') : extension.description;
 
-	extensionDiv.appendChild(nameDiv);
-	extensionDiv.appendChild(descDiv);
-	container.appendChild(extensionDiv);
-
+	//  create an aptions div which is part of the left div
+	//  <div class="options">
 	var optionsDiv = document.createElement('div');
 
 	var checkbox = document.createElement('input');
@@ -49,15 +51,16 @@ function display(extension) {
 	checkbox.addEventListener('click', function() {
 		if (this.checked) {
 			chrome.management.setEnabled(this.id, true);
-			this.parentNode.parentNode.classList.add('extension-div-enabled')
-			this.parentNode.parentNode.classList.remove('extension-div-disabled');
+			this.parentNode.parentNode.parentNode.classList.add('extension-div-enabled')
+			this.parentNode.parentNode.parentNode.classList.remove('extension-div-disabled');
+			this.nextSibling.innerHTML = "Enabled";
 		} else {
 			chrome.management.setEnabled(this.id, false);
-			this.parentNode.parentNode.classList.add('extension-div-disabled')
-			this.parentNode.parentNode.classList.remove('extension-div-enabled');
+			this.parentNode.parentNode.parentNode.classList.add('extension-div-disabled')
+			this.parentNode.parentNode.parentNode.classList.remove('extension-div-enabled');
+			this.nextSibling.innerHTML = "Disabled";
 		}
 	}, false);
-
 
 	var label = document.createElement('label');
 	label.setAttribute('for', extension.id);
@@ -66,14 +69,28 @@ function display(extension) {
 	optionsDiv.appendChild(checkbox);
 	optionsDiv.appendChild(label);
 
-	extensionDiv.appendChild(optionsDiv);
+	var leftDiv = document.createElement('div');
+	leftDiv.classList.add('left-content');
 
-			// <div class="extension-div">
-			// 	<div class="name-div">Grammarly</div>
-			// 	<div class="desc-div">this extension does this</div>
-			// 	<div class="options">
-			// 		<input type="checkbox" id="chr1">
-			// 		<label for="chr1">Enabled</label>
-			// 	</div>
-			// </div>
+	leftDiv.appendChild(descDiv);
+	leftDiv.appendChild(optionsDiv);
+
+	extensionDiv.appendChild(leftDiv);
+
+	// create and append the rightDiv
+	// <div class="right-img">
+	// 		<img src="https://i.stack.imgur.com/YDRAT.jpg?s=48&g=1" alt="">
+	// </div>
+	var rightDiv = document.createElement('div');
+	rightDiv.classList.add('right-img');
+	var img = document.createElement('img');
+	img.setAttribute('src', extension.icons.length == 1 ? extension.icons[0].url : extension.icons[1].url);
+	rightDiv.appendChild(img);
+	extensionDiv.appendChild(rightDiv);
+
+	container.appendChild(extensionDiv);
 }
+
+document.getElementById('github-icon').addEventListener('click', function() {
+	window.open('https://github.com/aedorado/ext-control-panel');
+}, false);
