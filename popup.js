7f06f782ca5ab3df492bac6c88b0ba1f5681fed5
Anurag El Dorado <annurag94@gmail.com>
2016-08-14 02:27:16 +0530

    Rewritten code in reactjs.

--- popup.js
+++ popup.js
@@ -9,98 +9,72 @@ chrome.management.getAll(function(allExtensions) {
 	        return 1;
 	    }
 	});
-
-	allExtensions.forEach(function(extension) {
-		display(extension);
-	});
+	ReactDOM.render(<AllExt extensions={allExtensions} />, document.getElementById("container"));
 });
 
-function display(extension, namelike='') {
-	var container = document.getElementById('container');
+var AllExt = React.createClass({
+	render: function() {
+		var rows = [];
 
-	var extensionDiv = document.createElement('div');
-	extensionDiv.classList.add(extension.enabled ? 'extension-div-enabled': 'extension-div-disabled');
-	extensionDiv.classList.add('extension-div');
-	extensionDiv.setAttribute('name', extension.name.toLowerCase());
+		this.props.extensions.forEach(function(ext, i) {
+			rows.push(	
+			<div className={"extension-div " + (ext.enabled ? 'extension-div-enabled' : 'extension-div-disabled')} name={ext.name.toLowerCase()}>
+				<div className="name-div">{ext.name}</div>
+				<div className="row">
+					<div className="left-div"><img src={ext.icons.length == 1 ? ext.icons[0].url : ext.icons[1].url} /></div>
+					<div className="mid-div" title={ext.description}>{ext.description.length >= 90 ? (ext.description.substring(0, 90) + '...') : (ext.description.length ? ext.description : 'NO DESC')}</div>
+					<div className="right-div" id={ext.id}><DelImg extid={ext.id} /><ToggleImg extid={ext.id} enabled={ext.enabled}/></div>
+				</div>
+			</div>
+			);
+		});
 
-	// create and append the name div
-	var nameDiv = document.createElement('div');
-	nameDiv.classList.add('name-div');
-	nameDiv.innerHTML = extension.name;
-	extensionDiv.appendChild(nameDiv);
+		return (<div>{rows}</div>)
+	}
+});
 
-	var row = document.createElement('div');
-	row.classList.add('row');
+var ToggleImg = React.createClass({
 
-	var leftDiv = document.createElement('div');
-	leftDiv.classList.add('left-div');
-	var img = document.createElement('img');
-	img.setAttribute('src', extension.icons.length == 1 ? extension.icons[0].url : extension.icons[1].url);
-	leftDiv.appendChild(img);
-	row.appendChild(leftDiv);
-
-	var midDiv = document.createElement('div');
-	midDiv.classList.add('mid-div');
-	midDiv.setAttribute('title', extension.description);
-	midDiv.innerHTML = extension.description.length >= 90 ? (extension.description.substring(0, 90) + '...') : extension.description;
-	midDiv.innerHTML += (extension.description.length == 0) ? 'No description provided by app maker.': '';
-	row.appendChild(midDiv);
-
-	var rightDiv = document.createElement('div');
-	rightDiv.classList.add('right-div');
-
-	var startButton = document.createElement('img');
-	startButton.setAttribute('class', 'start-button');
-	startButton.setAttribute('title', (extension.enabled ? 'Turn Off': 'Turn On'));
-	startButton.setAttribute('src', 'icons/power.svg');
-	startButton.setAttribute('id', 'start-' + extension.id)
-	startButton.addEventListener('click', function() {
-		var element = this;
-		chrome.management.get(this.id.substring(6), function(ext) {
+	clickHandler: function() {
+		var idToToggle = this.props.extid;
+		chrome.management.get(idToToggle, function(ext) {
 			if (ext.enabled) {
-				chrome.management.setEnabled(this.args[0], false);
-				element.parentNode.parentNode.parentNode.classList.add('extension-div-disabled')
-				element.parentNode.parentNode.parentNode.classList.remove('extension-div-enabled');
+				chrome.management.setEnabled(idToToggle, false);
+				document.getElementById('toggle-' + idToToggle).parentNode.parentNode.parentNode.classList.add('extension-div-disabled')
+				document.getElementById('toggle-' + idToToggle).parentNode.parentNode.parentNode.classList.remove('extension-div-enabled');
 			} else {
-				chrome.management.setEnabled(this.args[0], true);
-				element.parentNode.parentNode.parentNode.classList.add('extension-div-enabled')
-				element.parentNode.parentNode.parentNode.classList.remove('extension-div-disabled');
+				chrome.management.setEnabled(idToToggle, true);
+				document.getElementById('toggle-' + idToToggle).parentNode.parentNode.parentNode.classList.add('extension-div-enabled')
+				document.getElementById('toggle-' + idToToggle).parentNode.parentNode.parentNode.classList.remove('extension-div-disabled');
 			}
 		});
-	}, false);
+	},
 
-	var deleteButton = document.createElement('img');
-	deleteButton.setAttribute('class', 'delete-button');
-	deleteButton.setAttribute('src', 'icons/garbage.svg');
-	deleteButton.setAttribute('id', 'delete-' + extension.id)
-	deleteButton.addEventListener('click', function() {
-		var idToRemove = this.id.substring(7);
-		var nodeToRemove = this.parentNode.parentNode.parentNode;
-		chrome.management.uninstall(this.id.substring(7));
+	render: function() {
+		return (
+			<img onClick={this.clickHandler} className="start-button" title={"Turn " + (this.props.enabled ? ' Off' : 'On')} src="icons/power.svg" id={"toggle-" + this.props.extid} />
+		)
+	}
+});
+
+var DelImg = React.createClass({
+
+	clickHandler: function() {
+		var idToRemove = this.props.extid;
+		var nodeToRemove = document.getElementById('delete-' + idToRemove).parentNode.parentNode.parentNode;
+		chrome.management.uninstall(this.props.extid);
 		chrome.management.onUninstalled.addListener(function(idToRemove) {
 			nodeToRemove.parentNode.removeChild(nodeToRemove);
 		});
-	}, false);
-	deleteButton.addEventListener('mouseover', function() {
-		this.setAttribute('src', 'icons/garbage-hover.svg');	
-	}, false);
-	deleteButton.addEventListener('mouseout', function() {
-		this.setAttribute('src', 'icons/garbage.svg');			
-	}, false);
+	},
 
-	rightDiv.appendChild(deleteButton);
-	rightDiv.appendChild(startButton);
-	rightDiv.setAttribute('id', extension.id);
+	render: function() {
+		return (
+			<img onClick={this.clickHandler} className="delete-button" src="icons/garbage.svg" id={"delete-" + this.props.extid} />
+		)
+	}
+});
 
-	row.appendChild(rightDiv);
-
-	extensionDiv.appendChild(row);
-	container.appendChild(extensionDiv);
-}
-
-document.getElementById('github-icon').addEventListener('click', function() {
-	window.open('https://github.com/aedorado/ext-control-panel');
-}, false);
 
 document.getElementById('search').addEventListener('keyup', function(e) {
 	var searchTerm = e.target.value.toLowerCase();
@@ -109,10 +83,8 @@ document.getElementById('search').addEventListener('keyup', function(e) {
 		var extensionName = ext.getAttribute('name');
 		if (extensionName.indexOf(searchTerm) == -1) {
 			ext.classList.add('hide');			
-			// ext.classList.add('shrink-and-disappear');
 		} else {
 			ext.classList.remove('hide');						
-			// ext.classList.remove('shrink-and-disappear');
 		}
 	});
 }, false);
